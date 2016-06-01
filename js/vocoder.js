c6ad13be1184651e24c5db03b9a00f1ec55006ca
Thilo Ilg <contact@thiloilg.com>
2016-06-01 19:08:21 +0200

    switch advice

--- js/vocoder.js
+++ js/vocoder.js
@@ -3,66 +3,45 @@
 */
 var vocoder = (function(){
 
-    var bs = 512; // frame buffer size
+    var bs = 4096; // frame buffer size
     var sf = 44100; // sampling frequency
     var numIn = 2; // number of input buffer channel
     var numOut = 1; // number of output buffer channel
 
     var node = context.createScriptProcessor(bs, numIn, numOut);
 
-    var fftBuffer = new complex_array.ComplexArray(bs);
+    var fftBuffer, buffIn, buffOut, dataIn, dataOut, filtered;
 
     node.onaudioprocess = function(evt){
 
-        var buffIn = evt.inputBuffer;
-        var buffOut = evt.outputBuffer;
+        fftBuffer = new complex_array.ComplexArray(bs);
 
-        var dataOut = buffOut.getChannelData(0);
+        buffIn = evt.inputBuffer;
+        buffOut = evt.outputBuffer;
 
-        var dataIn = new Array(numIn);
+        dataOut = buffOut.getChannelData(0);
 
-        for(var i = 0; i < buffIn.numberOfChannels; i++){
-            dataIn[i] = buffIn.getChannelData(i);
-        }
+        dataIn = new Array(numIn);
+
+        dataIn[0] = buffIn.getChannelData(0);
+        dataIn[1] = buffIn.getChannelData(1);
 
         fftBuffer.map(function(sample, s, len){
-            var temp = (dataIn[0][s] + dataIn[1][s]) / 2;
-            //sample.real = temp;
-            //sample.imag = temp;
-        });
-
-        var filtered = fftBuffer.frequencyMap(function(freq, i, n) {
-
-        });
-
-        /**
-        fftBuffer.map(function(sample, s, length){
-            var temp = (dataIn[0][s]); // + dataIn[1][s]) / 2.0;
+            var temp = dataIn[0][s];
             sample.real = temp;
             sample.imag = temp;
         });
 
-        var frequencies = fftBuffer.FFT();
-
-        
-        frequencies.map(function(freq, i, length){
-            if (i < length/2) {
+        filtered = fftBuffer.frequencyMap(function(freq, i, n) {
+            if (i > n/5 && i < 4*n/5) {
                 freq.real = 0;
                 freq.imag = 0;
             }
         });
-        **/
 
         filtered.map(function(sample, s, length){
             dataOut[s] = sample.real;
         });
-
-        /**
-        for(var s = 0; s < bs; s++){
-            dataOut[s] = (dataIn[0][s] + dataIn[1][s]) / 2.0;
-        }
-        **/
-
     }
 
     return node;
